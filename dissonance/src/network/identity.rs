9c21bd5dc8d06f09a6b37531a83edc6f6b6d43c7
magus96 <anirudh5462@gmail.com>
2025-09-13 20:47:08 +0530

    Added persistent node identity, storing node identity locally

---
+++ dissonance/src/network/identity.rs
@@ -0,0 +1,101 @@
+use ed25519_dalek::{SigningKey,VerifyingKey, SECRET_KEY_LENGTH, PUBLIC_KEY_LENGTH, KEYPAIR_LENGTH};
+use libp2p::{identity, PeerId};
+use rand::TryRngCore;
+use serde::{Serialize, Deserialize};
+use std::fs;
+use std::path::{Path, PathBuf};
+use anyhow::{Context, Result};
+
+#[derive(Debug, Clone)]
+pub struct NodeIdentity{
+    pub signing_key: SigningKey,
+    pub verifying_key: VerifyingKey,
+    pub peer_id: PeerId
+}
+
+#[derive(Serialize, Deserialize)]
+struct StoredNodeIdentity{
+    private_key_bytes: [u8; SECRET_KEY_LENGTH],
+}
+
+impl NodeIdentity{
+
+    pub fn get_identity() -> Result<Self>{
+        let identity_path = Self::get_identity_path()?;
+
+        if identity_path.exists(){
+            println!("loading existing identity from keypair: {}", identity_path.display());
+            Self::load_from_file(&identity_path)
+        }else{
+            println!("Generating new identity!");
+            let identity = Self::load_or_generate()?;
+            identity.save_to_file(&identity_path)?;
+            println!("Generated new identity and stored at: {}", identity_path.display());
+            Ok(identity)
+        }
+    }
+
+    fn load_or_generate() -> Result<Self>{
+        let mut secret_bytes = [0u8; SECRET_KEY_LENGTH];
+        rand::rngs::OsRng.try_fill_bytes(&mut secret_bytes)?;
+        let signing_key: SigningKey = SigningKey::from_bytes(&secret_bytes);
+        let verifying_key = signing_key.verifying_key();
+        let lp2p_pub = identity::ed25519::PublicKey::try_from_bytes(&verifying_key.to_bytes()).context("Failed to cerate libp2p public key")?;
+        let peer_id = PeerId::from_public_key(&identity::PublicKey::from(lp2p_pub));
+        println!("Created Node identity: {}", peer_id);        
+        Ok(NodeIdentity { signing_key:signing_key, verifying_key: verifying_key, peer_id: peer_id })
+    }
+
+    fn save_to_file(&self, path: &Path) -> Result<()>{
+
+        if let Some(parent) = path.parent(){
+            fs::create_dir_all(parent).context("Failed to create identity directory")?;
+        }
+        
+        let stored = StoredNodeIdentity{
+            private_key_bytes : self.signing_key.to_bytes(),
+        };
+
+        let content = serde_json::to_string_pretty(&stored).context("failed to serialize identity")?;
+
+        fs::write(path, content).context("Failed to create identity file")?;
+        println!("Node identity save to: {}", path.display());
+        Ok(())
+    }
+
+    fn get_identity_path() -> Result<PathBuf>{
+        let cf_dir = dirs::config_dir().context("Could not determine config directory")?;
+        Ok(cf_dir.join("dsn-chat").join("node-identity.json"))
+    }
+
+    fn load_from_file(path: &Path) -> Result<Self>{
+        let content = fs::read_to_string(path).context("Failed to read identity file")?;
+        let stored :StoredNodeIdentity = serde_json::from_str(&content).context("Failed to parse identity")?;
+
+        let signing_key = SigningKey::from_bytes(&stored.private_key_bytes);
+        let verifying_key = signing_key.verifying_key();
+
+        let lp2p_pub = identity::ed25519::PublicKey::try_from_bytes(&verifying_key.to_bytes()).context("Could not create lp2p public key")?;
+        let peer_id = PeerId::from_public_key(&identity::PublicKey::from(lp2p_pub));
+
+        println!("Created node identity: {}", peer_id);
+        Ok(NodeIdentity { signing_key:signing_key, verifying_key: verifying_key, peer_id: peer_id })
+    }
+
+    pub fn peer_id(&self) -> PeerId{
+        self.peer_id
+    }
+
+    pub fn pub_key_bytes(&self) -> [u8; PUBLIC_KEY_LENGTH]{
+        self.verifying_key.to_bytes()
+    }
+
+    pub fn to_lp2p_keypair(&self) -> Result<identity::Keypair>{
+        let mut keypair_bytes = [0u8; KEYPAIR_LENGTH];
+        keypair_bytes[0..32].copy_from_slice(&self.signing_key.to_bytes());
+        keypair_bytes[32..64].copy_from_slice(&self.verifying_key.to_bytes());
+        let ed25519_keypair = identity::ed25519::Keypair::try_from_bytes(&mut keypair_bytes).context("Failed to create lp2p keypair")?;
+        Ok(identity::Keypair::from(ed25519_keypair))
+    }
+
+}
